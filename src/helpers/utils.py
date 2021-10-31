a00812b40c6491fc02b527d06099cbe089603a98
farreldarian <farreldarian@users.noreply.github.com>
2021-10-31 15:53:40 +0700

    Add listdir util

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 
 
 def make_dir_if_not_exists(path: str):
@@ -6,3 +7,8 @@ def make_dir_if_not_exists(path: str):
         return
 
     os.makedirs(path)
+
+
+def listdir(path: str, ignores=['.DS_Store']) -> List[str]:
+    items = os.listdir(path)
+    return [item for item in items if item not in ignores]
