c1d152b524721020ed77540e898a830459b058e4
farreldarian <farreldarian@users.noreply.github.com>
2021-11-07 20:03:43 +0700

    Use dataset class

---
+++ src/helpers/dataset.py
@@ -0,0 +1,74 @@
+import os
+import yaml
+import numpy as np
+from pathlib import Path
+from PIL import Image
+from .utils import listdir
+
+HELPERS_FOLDER = Path(__file__).parent
+CONFIG_PATH = HELPERS_FOLDER.joinpath('../../configs/dataset.yml')
+
+
+class Dataset:
+    def __init__(self, path=CONFIG_PATH):
+        self.yaml_config = self.__load_config(path)
+
+    @staticmethod
+    def resolve_collection_path(collection: str):
+        return f'dataset/{collection}'
+
+    @staticmethod
+    def resolve_image_path(collection: str, image_file: str):
+        return f'{Dataset.resolve_collection_path(collection)}/{image_file}'
+
+    def categories(self, ignores=None):
+        categories = list(self.yaml_config['collections_per_category'].keys())
+        if ignores is not None:
+            categories = [
+                category for category in categories if category not in ignores]
+        return categories
+
+    def collections(self, category: str):
+        return list(self.yaml_config['collections_per_category'][category])
+
+    def all_collections(self):
+        collections = []
+        for category in self.categories():
+            collections += self.collections(category)
+
+        return collections
+
+    def collection_category(self, collection: str):
+        categories = self.categories()
+        for category in categories:
+            collections = self.collections(category)
+            if collection in collections:
+                return category
+
+        return None
+
+    def collection_images(self, collection: str, ignores=None):
+        images = listdir(self.resolve_collection_path(collection))
+        if ignores is not None:
+            images = [image for image in images if image not in ignores]
+        return images
+
+    def load_image(self, collection: str, image_file: str):
+        image_path = self.resolve_image_path(collection, image_file)
+        return np.array(Image.open(image_path))
+
+    def save_dir(self):
+        return self.yaml_config['save_dir']
+
+    def allowed_extensions(self):
+        return self.yaml_config['allowed_extensions']
+
+    def tarimage_per_collection(self):
+        return self.yaml_config['target']['image_per_collection']
+
+    def __load_config(self, path):
+        if not os.path.isfile(path):
+            raise Exception(f"Can't locate config path in {path}")
+
+        with open(path) as f:
+            return yaml.load(f, Loader=yaml.FullLoader)
