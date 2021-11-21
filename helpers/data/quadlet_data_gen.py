48272b676b77f870e86985a24cbbf1370880121a
farreldarian <farreldarian@users.noreply.github.com>
2021-11-21 17:35:56 +0700

    Add types

--- helpers/data/quadlet_data_gen.py
+++ helpers/data/quadlet_data_gen.py
@@ -4,9 +4,9 @@ from ..dataset import Dataset
 
 
 class QuadletDataGen(tf.keras.utils.Sequence):
-    def __init__(self, dataset=Dataset(), batch_size=64):
-        self.dataset = dataset
-        self.batch_size = batch_size
+    def __init__(self, dataset=Dataset(), batch_size=64) -> None:
+        self.dataset: Dataset = dataset
+        self.batch_size: int = batch_size
 
-    def __len__(self):
+    def __len__(self) -> int:
         return math.ceil(len(self.dataset.all_images()) / self.batch_size)
