fca209d45573dd5837622749554eb0c60aca3dce
farreldarian <farreldarian@users.noreply.github.com>
2021-11-21 17:52:33 +0700

    Quadlet datage

--- helpers/data/quadlet_data_gen.py
+++ helpers/data/quadlet_data_gen.py
@@ -1,6 +1,9 @@
 import math
 import tensorflow as tf
-from ..dataset import Dataset
+import numpy as np
+
+from helpers.quadlet import generate_quadlet
+from helpers.dataset import Dataset
 
 
 class QuadletDataGen(tf.keras.utils.Sequence):
@@ -10,3 +13,17 @@ class QuadletDataGen(tf.keras.utils.Sequence):
 
     def __len__(self) -> int:
         return math.ceil(len(self.dataset.all_images()) / self.batch_size)
+
+    def __getitem__(self, idx) -> np.ndarray:
+        start = idx * self.batch_size
+        end = (idx + 1) * self.batch_size
+
+        batch_x = [
+            generate_quadlet(nft.collection, nft.image_name)
+            for nft in self.dataset.nfts[start:end]
+        ]
+
+        # Not used
+        batch_y = [[0]] * len(batch_x)
+
+        return np.array(batch_x), np.array(batch_y)
