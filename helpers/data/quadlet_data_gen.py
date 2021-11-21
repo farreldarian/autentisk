c74a7f89c6d7caeec8769676c48148d64ba782d5
farreldarian <farreldarian@users.noreply.github.com>
2021-11-21 17:58:07 +0700

    Add custom preprocess func

--- helpers/data/quadlet_data_gen.py
+++ helpers/data/quadlet_data_gen.py
@@ -1,15 +1,17 @@
 import math
 import tensorflow as tf
 import numpy as np
+from helpers.data.nft import NFT
 
 from helpers.quadlet import generate_quadlet
 from helpers.dataset import Dataset
 
 
 class QuadletDataGen(tf.keras.utils.Sequence):
-    def __init__(self, dataset=Dataset(), batch_size=64) -> None:
+    def __init__(self, dataset=Dataset(), batch_size=64, preprocess_func=None) -> None:
         self.dataset: Dataset = dataset
         self.batch_size: int = batch_size
+        self.preprocess_func = preprocess_func
 
     def __len__(self) -> int:
         return math.ceil(len(self.dataset.all_images()) / self.batch_size)
@@ -19,7 +21,7 @@ class QuadletDataGen(tf.keras.utils.Sequence):
         end = (idx + 1) * self.batch_size
 
         batch_x = [
-            generate_quadlet(nft.collection, nft.image_name)
+            self.__generate_quadlet(nft)
             for nft in self.dataset.nfts[start:end]
         ]
 
@@ -27,3 +29,9 @@ class QuadletDataGen(tf.keras.utils.Sequence):
         batch_y = [[0]] * len(batch_x)
 
         return np.array(batch_x), np.array(batch_y)
+
+    def __generate_quadlet(self, nft: NFT):
+        quadlet = generate_quadlet(nft.collection, nft.image_name)
+        if self.preprocess_func is not None:
+            return [self.preprocess_func(img) for img in quadlet]
+        return quadlet
