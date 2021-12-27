62c69429079ea896041e52b44e49706b6873a1e7
farreldarian <farreldarian@users.noreply.github.com>
2021-12-27 17:27:57 +0700

    Replace collection struct with address

--- app/protocol/contracts/Autentisk.sol
+++ app/protocol/contracts/Autentisk.sol
@@ -4,36 +4,21 @@ pragma solidity ^0.8.11;
 import "./AutentiskERC721.sol";
 
 contract Autentisk {
-    using Counters for Counters.Counter;
+    event CollectionCreated(address indexed collectionAddress);
 
-    struct Collection {
-        AutentiskERC721 collection;
-        address owner;
-    }
-
-    event CollectionCreated(uint256 indexed collectionId);
-
-    Counters.Counter private collectionIds;
-    mapping(uint256 => Collection) collections;
+    mapping(AutentiskERC721 => address) collectionOwners;
     uint256 public totalCollection;
 
     constructor() {}
 
     function createCollection(string calldata name, string calldata symbol)
         external
-        returns (uint256 id)
     {
-        collectionIds.increment();
-
-        id = collectionIds.current();
         AutentiskERC721 collection = new AutentiskERC721(name, symbol);
 
-        collections[id] = Collection({
-            collection: collection,
-            owner: msg.sender
-        });
+        collectionOwners[collection] = msg.sender;
         totalCollection++;
 
-        emit CollectionCreated(id);
+        emit CollectionCreated(address(collection));
     }
 }
