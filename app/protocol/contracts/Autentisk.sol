2da035973f20e43ad97c1bd9fe88762b15cabc55
farreldarian <farreldarian@users.noreply.github.com>
2021-12-27 17:55:00 +0700

    Add chainlink oracle

--- app/protocol/contracts/Autentisk.sol
+++ app/protocol/contracts/Autentisk.sol
@@ -1,10 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./dependencies/ChainlinkClient.sol";
 import "./AutentiskERC721.sol";
 
-contract Autentisk {
+contract Autentisk is Ownable, ChainlinkClient {
+    using Chainlink for Chainlink.Request;
+
     event CollectionCreated(address indexed collectionAddress);
+    event OracleChanged(
+        address indexed previousOracle,
+        address indexed newOracle,
+        bytes32 jobId
+    );
 
     modifier onlyCollectionOwner(AutentiskERC721 collection) {
         require(
@@ -17,7 +26,18 @@ contract Autentisk {
     mapping(AutentiskERC721 => address) collectionOwners;
     uint256 public totalCollection;
 
-    constructor() {}
+    address public oracle;
+    bytes32 public jobId;
+    uint256 public fee;
+
+    constructor(
+        address _oracle,
+        bytes32 _jobId,
+        uint256 _fee
+    ) {
+        setPublicChainlinkToken();
+        setOracle(_oracle, _jobId, _fee);
+    }
 
     function createCollection(string calldata name, string calldata symbol)
         external
@@ -36,5 +56,21 @@ contract Autentisk {
         returns (uint256)
     {
         return collection.mint(tokenURI);
+    }
+
+    function setOracle(
+        address _oracle,
+        bytes32 _jobId,
+        uint256 _fee
+    ) public onlyOwner {
+        require(_oracle != 0, "Invalid oracle address");
+
+        address prevOracle = oracle;
+
+        oracle = _oracle;
+        jobId = _jobId;
+        fee = _fee;
+
+        emit OracleChanged(prevOracle, _oracle, _jobId);
     }
 }
