de2792f489f97f2a64cd33a88efbfa69f0535edc
farreldarian <farreldarian@users.noreply.github.com>
2021-12-29 02:05:57 +0700

    Add polygon and hardhat-deploy

--- app/protocol/hardhat.config.ts
+++ app/protocol/hardhat.config.ts
@@ -1,5 +1,5 @@
 import * as dotenv from "dotenv";
-
+import "hardhat-deploy";
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
@@ -25,8 +25,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    polygon: {
+      url: "https://polygon-rpc.com",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
