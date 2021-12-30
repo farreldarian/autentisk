4866878842c0ab012fb119bab7a5570c727562a3
farreldarian <farreldarian@users.noreply.github.com>
2021-12-30 12:04:59 +0700

    use function based augment

--- helpers/augly.py
+++ helpers/augly.py
@@ -1,11 +1,16 @@
 from PIL import Image
 import augly.image as imaugs
+import random
 
-creative_trans = [imaugs.OverlayText(),
-                  imaugs.OverlayEmoji(),
-                  imaugs.MemeFormat()]
+
+def overlay_text(img): return imaugs.overlay_text(img)
+def overlay_emoji(img): return imaugs.overlay_emoji(img)
+def meme_format(img): return imaugs.meme_format(img)
+
+
+transform_functions = [overlay_text, overlay_emoji, meme_format]
 
 
 def augly_augment(image: Image) -> Image:
-    transform = imaugs.OneOf(creative_trans)
+    transform = random.choice(transform_functions)
     return transform(image)
