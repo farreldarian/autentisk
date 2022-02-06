4c52e4db48691a91cbd4c38342e11562dcc07533
farreldarian <farreldarian@users.noreply.github.com>
2022-02-06 10:31:55 +0700

    Add oracle

--- app/protocol/contracts/Autentisk.sol
+++ app/protocol/contracts/Autentisk.sol
@@ -1,19 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "./dependencies/ChainlinkClient.sol";
 import "./AutentiskERC721.sol";
 
-contract Autentisk is Ownable, ChainlinkClient {
-    using Chainlink for Chainlink.Request;
-
+contract Autentisk {
     event CollectionCreated(address indexed collectionAddress);
-    event OracleChanged(
-        address indexed previousOracle,
-        address indexed newOracle,
-        bytes32 jobId
-    );
 
     modifier onlyCollectionOwner(AutentiskERC721 collection) {
         require(
@@ -26,6 +17,9 @@ contract Autentisk is Ownable, ChainlinkClient {
     mapping(AutentiskERC721 => address) collectionOwners;
     uint256 public totalCollection;
 
+    string public classifierUrl;
+    uint256 public similarityThreshold;
+
     address public oracle;
     bytes32 public jobId;
     uint256 public fee;
@@ -33,10 +27,14 @@ contract Autentisk is Ownable, ChainlinkClient {
     constructor(
         address _oracle,
         bytes32 _jobId,
-        uint256 _fee
+        uint256 _fee,
+        string memory _classifierUrl,
+        uint256 _similarityThreshold
     ) {
         setPublicChainlinkToken();
         setOracle(_oracle, _jobId, _fee);
+        classifierUrl = _classifierUrl;
+        similarityThreshold = _similarityThreshold;
     }
 
     function createCollection(string calldata name, string calldata symbol)
@@ -54,8 +52,37 @@ contract Autentisk is Ownable, ChainlinkClient {
         external
         onlyCollectionOwner(collection)
         returns (uint256)
+    {}
+
+    function checkAuthenticity(bytes32[] calldata tokenUris) external {
+        require(tokenUris.length > 0, "Token URI can't be empty");
+
+        Chainlink.Request memory request = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfill.selector
+        );
+
+        request.add("get", makeRequestUrl(tokenUris));
+
+        return sendChainlinkRequestTo(oracle, request, fee);
+    }
+
+    function fulfillAuthenticity(bytes32 requestId, bytes calldata data)
+        public
+        recordChainlinkFulfillment(requestId)
     {
-        return collection.mint(tokenURI);
+        (
+            AutentiskERC721 collection,
+            string calldata tokenURI,
+            uint256 closestSimilarity
+        ) = abi.decode(data, (address, string, uint256));
+
+        if (isSimilar(closestSimilarity)) {
+            revert("");
+        }
+
+        collection.mint();
     }
 
     function setOracle(
@@ -72,5 +99,20 @@ contract Autentisk is Ownable, ChainlinkClient {
         fee = _fee;
 
         emit OracleChanged(prevOracle, _oracle, _jobId);
+    }
+
+    function isSimilar(uint256 similarity) private returns (bool) {
+        return similarity <= similarityThreshold;
+    }
+
+    function makeRequestUrl(bytes32[] calldata tokenUris)
+        private
+        pure
+        returns (string memory url)
+    {
+        url = abi.encodePacked(classifierUrl, "?");
+        for (uint256 i = 0; i < tokenUris.length; i++) {
+            url = abi.encodePacked("tokenUri[]=", string(tokenUris[i]));
+        }
     }
 }
