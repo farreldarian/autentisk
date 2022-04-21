2bddbb6fd18c6ec5c88e77ecbc818e47135ddab9
farreldarian <farreldarian@users.noreply.github.com>
2022-04-21 14:17:37 +0700

    Add fulfill mint

--- apps/protocol/contracts/marketplace/Autentisk.sol
+++ apps/protocol/contracts/marketplace/Autentisk.sol
@@ -1,15 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./AuthenticityRegistry.sol";
 import "../token/AutentiskERC721.sol";
 
-contract Autentisk is ChainlinkClient, Ownable {
-    using Chainlink for Chainlink.Request;
-
+contract Autentisk is Ownable {
     event CollectionCreated(address indexed collectionAddress);
-    event OracleChanged(address prevOracle, address newOracle, bytes32 jobId);
 
     modifier onlyCollectionOwner(AutentiskERC721 collection) {
         require(
@@ -19,27 +16,21 @@ contract Autentisk is ChainlinkClient, Ownable {
         _;
     }
 
+    address immutable AUTHENTICITY_REGISTRY;
+
     mapping(AutentiskERC721 => address) collectionOwners;
     uint256 public totalCollection;
 
-    string public classifierUrl;
-    uint256 public similarityThreshold;
+    constructor(address authenticityRegistry) {
+        AUTHENTICITY_REGISTRY = authenticityRegistry;
+    }
 
-    address public oracle;
-    bytes32 public jobId;
-    uint256 public fee;
-
-    constructor(
-        address _oracle,
-        bytes32 _jobId,
-        uint256 _fee,
-        string memory _classifierUrl,
-        uint256 _similarityThreshold
-    ) {
-        setPublicChainlinkToken();
-        setOracle(_oracle, _jobId, _fee);
-        classifierUrl = _classifierUrl;
-        similarityThreshold = _similarityThreshold;
+    modifier onlyAuthenticityRegistry() {
+        require(
+            msg.sender == AUTHENTICITY_REGISTRY,
+            "Autentisk: Not AuthenticityRegistry"
+        );
+        _;
     }
 
     function createCollection(string calldata name, string calldata symbol)
@@ -56,71 +47,14 @@ contract Autentisk is ChainlinkClient, Ownable {
     function mint(AutentiskERC721 collection, string calldata tokenURI)
         external
         onlyCollectionOwner(collection)
-        returns (uint256)
-    {}
+    {
+        AuthenticityRegistry(AUTHENTICITY_REGISTRY).checkAuthenticity(tokenURI, collection)
+    }
 
-    function checkAuthenticity(string[] calldata tokenUris)
+    function fulfillMint(AutentiskERC721 collection, string calldata tokenURI)
         external
-        returns (bytes32 requestId)
+        onlyAuthenticityRegistry
     {
-        require(tokenUris.length > 0, "Token URI can't be empty");
-
-        Chainlink.Request memory request = buildChainlinkRequest(
-            jobId,
-            address(this),
-            this.fulfillAuthenticity.selector
-        );
-
-        request.add("get", makeRequestUrl(tokenUris));
-
-        return sendChainlinkRequestTo(oracle, request, fee);
-    }
-
-    function fulfillAuthenticity(bytes32 requestId, bytes calldata data)
-        public
-        recordChainlinkFulfillment(requestId)
-    {
-        (
-            AutentiskERC721 collection,
-            string memory tokenURI,
-            uint256 closestSimilarity
-        ) = abi.decode(data, (AutentiskERC721, string, uint256));
-
-        if (isSimilar(closestSimilarity)) {
-            revert("");
-        }
-
         collection.mint(tokenURI);
-    }
-
-    function setOracle(
-        address _oracle,
-        bytes32 _jobId,
-        uint256 _fee
-    ) public onlyOwner {
-        require(_oracle != address(0), "Invalid oracle address");
-
-        address prevOracle = oracle;
-
-        oracle = _oracle;
-        jobId = _jobId;
-        fee = _fee;
-
-        emit OracleChanged(prevOracle, _oracle, _jobId);
-    }
-
-    function isSimilar(uint256 similarity) private returns (bool) {
-        return similarity <= similarityThreshold;
-    }
-
-    function makeRequestUrl(string[] memory tokenUris)
-        private
-        view
-        returns (string memory url)
-    {
-        url = string(abi.encodePacked(classifierUrl, "?"));
-        for (uint256 i = 0; i < tokenUris.length; i++) {
-            url = string(abi.encodePacked(url, "tokenUri[]=", tokenUris[i]));
-        }
     }
 }
