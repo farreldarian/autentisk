275044870f798f968ea35f20d3e83887b7eed652
farreldarian <farreldarian@users.noreply.github.com>
2022-04-21 14:19:54 +0700

    Update sol version

--- apps/protocol/contracts/marketplace/Autentisk.sol
+++ apps/protocol/contracts/marketplace/Autentisk.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.11;
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./AuthenticityRegistry.sol";
