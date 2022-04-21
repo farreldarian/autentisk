73b58732531d4b20c6a6fe85bff9bf09a1e82234
farreldarian <farreldarian@users.noreply.github.com>
2022-04-21 15:59:55 +0700

    Fix mapping

--- apps/subgraph/src/mapping.ts
+++ apps/subgraph/src/mapping.ts
@@ -6,7 +6,7 @@ export function handleCollectionCreated(event: CollectionCreated): void {
   const collection = new Collection(event.params.collectionAddress.toHex());
   collection.owner = event.transaction.from.toHex();
 
-  const nft = AutentiskERC721.bind(event.transaction.from);
+  const nft = AutentiskERC721.bind(event.params.collectionAddress);
   collection.name = nft.name();
   collection.symbol = nft.symbol();
   collection.save();
