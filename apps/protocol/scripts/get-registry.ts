5d5465b4cbaa7843af2804906ceb2dfe97076859
farreldarian <farreldarian@users.noreply.github.com>
2022-04-28 13:06:40 +0700

    Create get registry script

---
+++ apps/protocol/scripts/get-registry.ts
@@ -0,0 +1,12 @@
+import { ethers } from "hardhat";
+import { Autentisk } from "../typechain";
+
+async function main() {
+  const autentisk = (await ethers.getContract("Autentisk")) as Autentisk;
+  console.log(await autentisk.AUTHENTICITY_REGISTRY());
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
