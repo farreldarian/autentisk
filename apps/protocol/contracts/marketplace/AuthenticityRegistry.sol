accc87154a096c6636c527d5b1f4eb2df79d0a4c
farreldarian <farreldarian@users.noreply.github.com>
2022-04-28 10:45:29 +0700

    Add license

--- apps/protocol/contracts/marketplace/AuthenticityRegistry.sol
+++ apps/protocol/contracts/marketplace/AuthenticityRegistry.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import {Chainlink, ChainlinkClient} from "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
