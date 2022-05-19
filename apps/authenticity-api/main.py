6bc1313d85c6aa38d074b6f3dcf161f422375404
farreldarian <farreldarian@users.noreply.github.com>
2022-05-19 11:54:20 +0700

    Compare similarity

--- apps/authenticity-api/main.py
+++ apps/authenticity-api/main.py
@@ -3,12 +3,13 @@ import numpy as np
 import uvicorn
 import os
 from fastapi import FastAPI
+from scipy.spatial.distance import cosine
 from core.unit import parse_ether
 from core.token_metadata import get_image_url
 from core.image import load_image
 from core.contract import get_request_id
 from core.encoder import get_encoder
-from core.s3 import get_vectors_key, upload_vector
+from core.s3 import get_vectors_key, upload_vector, download_vector
 from core.ipfs import get_cid
 
 app = FastAPI()
@@ -29,17 +30,20 @@ async def root(tokenUri: str = None):
     image = load_image(image_url)
 
     encoder = get_encoder()
-    query_vec = encoder([image])
+    query_vec = encoder(np.array([image]))
 
-    vec_list = get_vectors_key()
-    if len(vec_list) == 0:
+    vec_keys = get_vectors_key()
+    if len(vec_keys) == 0:
         upload_vector(query_vec, get_cid(tokenUri))
         return {"simialirty": 0}
     
-    dataset_vec = [download_vector()]
+    dataset_vec = [download_vector(key) for key in vec_keys]
+    closest = float('inf')
+    for vec in dataset_vec:
+        dist = cosine(query_vec, vec)
+        closest = np.min([dist, closest])
 
-
-    return {"similarity": parse_ether(1)}
+    return {"similarity": parse_ether(closest)}
 
 
 if __name__ == "__main__":
