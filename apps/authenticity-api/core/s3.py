a2f6a09e0ee9d46f874bc7c432304a11eafb26aa
farreldarian <farreldarian@users.noreply.github.com>
2022-05-19 11:33:10 +0700

    Fix get_vectors

--- apps/authenticity-api/core/s3.py
+++ apps/authenticity-api/core/s3.py
@@ -14,7 +14,7 @@ def get_client():
 
 def get_vectors(client=get_client()):
     res = client.list_objects_v2(Bucket=BUCKET_NAME,
-                                 Delimiter='/', Prefix="vectors"
+                                 Delimiter='/', Prefix="vectors/"
                                  )
     return res['Contents'] if ("Contents" in res.keys()) else []
 
