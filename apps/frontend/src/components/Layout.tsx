db476eb84a6a2635397e0c62165d00da0dd67c3a
farreldarian <farreldarian@users.noreply.github.com>
2022-05-26 12:00:05 +0700

    minH for layout

--- apps/frontend/src/components/Layout.tsx
+++ apps/frontend/src/components/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 }
 export default function Layout({ children }: LayoutProps) {
   return (
-    <Box bg="gray.100">
+    <Box bg="gray.100" minH="100vh">
       <Navbar />
       <Box>{children}</Box>
     </Box>
