c43382c08dd79228fc9cadee15f3889c6fba196d
farreldarian <farreldarian@users.noreply.github.com>
2022-05-31 14:48:13 +0700

    Redeploy

--- packages/contract/src/constants/deployed-contracts.ts
+++ packages/contract/src/constants/deployed-contracts.ts
@@ -1,12 +1,12 @@
 export default {
   [80001]: {
     'Autentisk': {
-      address: "0x25afEE2B019E8097796420244671F6b52F954B42",
-      startBlock: 26537859,
+      address: "0xFA5d10490c24A114BCf9d8949a2B680fB652f116",
+      startBlock: 26538577,
     },
     'AuthenticityRegistry': {
-      address: "0x67D25993d63fac42412Aab9B4bC81C45410f7B1e",
-      startBlock: 26537857,
+      address: "0x79E9E21E52aA11f3082ED7Da22BBed3aD2de5cc4",
+      startBlock: 26538575,
     },
   }
 }
