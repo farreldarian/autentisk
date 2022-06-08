3fa00fb4d1dc38f4b0ea182bdb2379459de7c79f
farreldarian <farreldarian@users.noreply.github.com>
2022-06-08 19:27:40 +0700

    Add logging

--- apps/authenticity-api/core/token_metadata.py
+++ apps/authenticity-api/core/token_metadata.py
@@ -4,8 +4,10 @@ from .ipfs import parse_if_ipfs
 
 
 def get_image_url(token_uri: str):
+    print("Fetching image... ", end="")
     url = parse_if_ipfs(token_uri)
     res = urlopen(url)
     metadata = json.loads(res.read())
+    print("[Done]")
     image = metadata["image"]
     return None if image is None else parse_if_ipfs(image)
