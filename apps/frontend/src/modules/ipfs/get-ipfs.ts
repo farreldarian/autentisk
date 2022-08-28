0c9611e0e02bcb5c81b65fb65c1855643f7f841a
edwinfernaldy <edwinfernaldy@gmail.com>
2022-08-28 18:08:11 +0700

    add ipfs auth

--- apps/frontend/src/modules/ipfs/get-ipfs.ts
+++ apps/frontend/src/modules/ipfs/get-ipfs.ts
@@ -3,11 +3,20 @@ import { create } from 'ipfs-http-client'
 // const IPFS_HOST = "api.thegraph.com/ipfs";
 const IPFS_HOST = 'ipfs.infura.io'
 
+const projectId = '2DyvrPekCFg9j8vHXZHQcQWbDCq'
+const projectSecret = 'd0ad0f4a5e387c071df6a33c443acb0e'
+
+const auth =
+  'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64')
+
 export default function getIpfs() {
   return create({
     host: IPFS_HOST,
     protocol: 'https',
     port: 5001,
     apiPath: '/api/v0',
+    headers: {
+      authorization: auth,
+    },
   })
 }
